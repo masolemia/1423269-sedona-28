083c305de579caa20e078408614f2593ac1ab073
Maria <mariasoldatenko@mail.ru>
2020-06-22 02:57:02 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var children = document.querySelector("[name=children]");
 
 var isStorageSupport = true;
 var storage = "";
+var isStorageSupportTwo = true;
+var storageTwo = "";
 
 try {
   storage = localStorage.getItem("adults");
@@ -14,22 +16,38 @@ try {
   isStorageSupport = false;
 };
 
+try {
+  storageTwo = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupportTwo = false;
+};
+
 searchButton.addEventListener("click", function (evt){
   evt.preventDefault();
   form.classList.toggle("search-form");
   form.classList.toggle("hidden-form");
+  form.classList.remove("error-form");
   dateEnter.focus();
   if(storage){
     adults.value=storage;
+  };
+  if(storageTwo){
+    children.value=storageTwo;
   }
 });
 
 form.addEventListener("submit", function(evt){
   if(!dateEnter.value || !dateLeave.value || !adults.value || !children.value){
     evt.preventDefault();
+    form.classList.remove("error-form");
+    form.offsetWidth=form.offsetWidth;
+    form.classList.add("error-form");
   }else{
     if(isStorageSupport){
-      localStorage.setItem("adultsNumber",adults.value);
+      localStorage.setItem("adults",adults.value);
+    };
+    if(isStorageSupportTwo){
+      localStorage.setItem("children", children.value);
     }
   }
 });
